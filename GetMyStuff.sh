7385274bbb4a10c8116b22e980fbfd127e3dae92
Mason <acmason1204@gmail.com>
2025-01-28 01:46:28 -0500

    Test commit

---
+++ GetMyStuff.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#
+echo "ID: $(id -u)"
+
+echo "Primary Group ID: $(id -g)"
+
+echo "Group: $(groups)"
+
+echo "Shell: $SHELL"
+
+echo "Home Directory: $HOME"
+
+echo "Path: $PATH"
+
